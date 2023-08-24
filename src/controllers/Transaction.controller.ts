8abb3a2c345746505d3fd092a0345e62bae7a439
Emilio Grisolia <emilio.grisolia@addalitica.com>
2023-08-24 17:47:04 -0300

    [UPDATE] Missing status codes in TransitionController

--- src/controllers/Transaction.controller.ts
+++ src/controllers/Transaction.controller.ts
@@ -1,31 +1,32 @@
 import { Express, NextFunction, Request, Response } from "express";
 import TransactionService from "../services/Transaction.service";
+import { DEFAULT_TIMEFRAME } from "../config/app";
+import { HTTP_BAD_REQUEST, HTTP_OK } from "../helpers/status_codes";
 
 
-const DEFAULT_TIMEFRAME: number = 30;
 
 export default (app: Express, transactionService: TransactionService): void => {
     app.get('/products/best_selling/by_units', async (request: Request, response: Response, next: NextFunction) => {
         const daysAgo = parseInt(request.query.daysAgo as string, 10) || 30;
         if (isNaN(daysAgo)) {
-            response.status(400).json({
+            response.status(HTTP_BAD_REQUEST).json({
                 error: `Invalid <daysAgo> query parameter. Expected a number, got ${request.query.daysAgo}`
             });
             return;
         }
         const products = await transactionService.bestSellingProductsByUnits(daysAgo);
-        response.json(products);
+        response.status(HTTP_OK).json(products);
     });
 
     app.get('/products/best_selling/by_value', async (request: Request, response: Response, next: NextFunction) => {
         const daysAgo = parseInt(request.query.daysAgo as string, 10) || DEFAULT_TIMEFRAME;
         if (isNaN(daysAgo)) {
-            response.status(400).json({
+            response.status(HTTP_BAD_REQUEST).json({
                 error: `Invalid <daysAgo> query parameter. Expected a number, got ${request.query.daysAgo}`
             });
             return;
         }
         const products = await transactionService.bestSellingProductsByValue(daysAgo);
-        response.json(products);
+        response.status(HTTP_OK).json(products);
     });
 }
