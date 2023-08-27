3b6bfc43410184844491d75c402a3009333a8e68
gemisa23 <77344920+gemisa23@users.noreply.github.com>
2023-08-27 18:17:32 -0300

    Update app.ts

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_TIMEFRAME: number = 30; //- The default timeframe for best selling products query.
 export const APP_PORT: number = 500;
 export const APP_ADDR: string = '127.0.0.1';
-export const DEFAULT_MONGO_URL: string = "mongodb://localhost:27017/SAM";
+export const DEFAULT_MONGO_URL: string = "mongodb://localhost:27017";
